b6b9039e9e5deae393284b84d788341412e23021
envykilIa <113392869+envykilIa@users.noreply.github.com>
2022-12-29 22:07:39 -0800

    Update egg_list.lua

--- egg_list.lua
+++ egg_list.lua
@@ -1,1 +1,65 @@
+--[[
+Starter World / AKA: Overworld
+Common Egg
+Spotted Egg
+Ice Shard Egg
+Spikey Egg
 
+Candy World:
+
+Ice Cream Egg
+Gummy Egg
+Slushy Egg
+Jelly Egg
+Dominus Egg
+
+Toy World:
+Wind-Up Egg
+Block Egg
+Toy Egg
+Rubber Egg
+Bee Egg
+Toxic Egg
+
+Beach World:
+
+Coconut Egg
+Sand Egg
+Beach Egg
+Ballon Egg
+Water Egg
+Crab Egg
+
+Atlantis World:
+
+Kelp Egg
+Ocean Egg
+Darkness Egg
+Coral Egg
+Ancient Egg
+Guardian Egg
+
+Rainbow World:
+
+Red Egg
+Orange Egg
+Colorful Egg
+Fancy Egg
+
+Underworld World:
+
+Stone Egg
+Obsidian Egg
+Fire Egg
+Evil Egg
+Dark Egg
+Sparkly Egg
+
+Mystic Forest World:
+
+Mossy Egg
+Spirit Egg
+Mythical Egg
+Mushroom Egg
+Pastel Egg
+--]]
